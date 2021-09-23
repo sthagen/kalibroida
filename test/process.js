f766ff42d999dea60de65ef446aaa9dbde063233
Stefan Hagen <stefan@hagen.link>
2021-09-23 06:58:11 +0200

    Update process.js

--- test/process.js
+++ test/process.js
@@ -4,7 +4,8 @@ const kalibroida = require('../src/process.js')
 
 const echo = (arg) => arg
 
-const vectors = require('./fixtures/process.json').forEach(e => e[0] = eval(e[0]))
+let vectors = require('./fixtures/process.json')
+vectors.forEach(e => e[0] = eval(e[0]))
 
 const Well = suite("bar")
 Well("foo", () => {
