931e548c28a7af3a463c58943066335a11fead5c
Stefan Hagen <stefan@hagen.link>
2021-09-23 07:04:18 +0200

    Update process.js

--- src/process.js
+++ src/process.js
@@ -2,17 +2,19 @@ const assert = require('uvu/assert');
 
 const process = (options) => {
   return vector => {
-    console.log(vector, options)
+		if (options.trace) console.log(vector, options)
     const method = typeof(options.method) === 'string'
       ? eval(options.method)
       : options.method
     const [function_under_test, parameters, expectation] = [
       typeof(vector[0]) === 'string' ? eval(vector[0]) : vector[0], vector.slice(1, -1), vector[vector.length - 1] ]
-    console.log("Parsed:")
-    console.log("       method: ", method)
-    console.log("          fut: ", function_under_test)
-    console.log("   parameters: ", parameters)
-    console.log("  expectation: ", expectation)
+    if (options.trace) {
+		  console.log("Parsed:")
+      console.log("       method: ", method)
+      console.log("          fut: ", function_under_test)
+      console.log("   parameters: ", parameters)
+      console.log("  expectation: ", expectation)
+		}
     return method(function_under_test(...parameters), expectation)
   }
 }
